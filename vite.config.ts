27a7301ed5df04b302dcb7e1f6f139cf797a7c31
ayoubqqqqq34-ship-it <ayoubqqqqq34@gmail.com>
2026-03-08 10:22:23 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite' // الركيزة الأساسية لـ Tailwind 4
-import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(), // تفعيل المحرك الجديد للستايل
-  ],
-  resolve: {
-    alias: {
-      // ربط الاختصار @ بمجلد src كما فعلنا في tsconfig
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-  server: {
-    // إعدادات اختيارية لتسهيل الوصول من الهاتف في الشبكة المحلية (جواب)
-    host: true,
-    port: 3000,
-  }
+  plugins: [react()],
 })
